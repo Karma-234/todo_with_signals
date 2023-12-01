dc481fcad63fcb07d2f2125cbd38cbfb59a1f4e8
Karma-234 <busari1kamal@gmail.com>
2023-12-01 10:20:53 -0800

    Added hints

--- lib/view/add_todo_view.dart
+++ lib/view/add_todo_view.dart
@@ -26,26 +26,34 @@ class AddTodoView extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            const TododInputField(),
-            12.v,
-            const TododInputField(),
-            const Spacer(),
-            ElevatedButton(
-              onPressed: () {},
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.black,
-                disabledBackgroundColor: Colors.black12,
-                disabledForegroundColor: Colors.black12.withOpacity(0.3),
-                shape: const ContinuousRectangleBorder(
-                    borderRadius: BorderRadius.all(
-                  Radius.circular(10),
-                )),
-              ),
-              child: Text(
-                'Continue',
-                style: GoogleFonts.markaziText(color: Colors.yellow),
-              ),
+            TododInputField(
+              hint: 'Title...',
+              onChanged: (v) => _todoTitle.value = v,
             ),
+            12.v,
+            TododInputField(
+              hint: 'Description...',
+              onChanged: (v) => _todoDescription.value = v,
+            ),
+            const Spacer(),
+            Watch((_) {
+              return ElevatedButton(
+                onPressed: _isValid.value ? () {} : null,
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.black,
+                  disabledBackgroundColor: Colors.black12,
+                  disabledForegroundColor: Colors.black12.withOpacity(0.3),
+                  shape: const ContinuousRectangleBorder(
+                      borderRadius: BorderRadius.all(
+                    Radius.circular(10),
+                  )),
+                ),
+                child: Text(
+                  'Continue',
+                  style: GoogleFonts.markaziText(color: Colors.yellow),
+                ),
+              );
+            }),
           ],
         ),
       ),
